57edced8f068b699998892c4f677d301e75ecf96
nirajmeshram <112005517+niraj-meshram@users.noreply.github.com>
2025-11-01 23:10:31 -0600

    Remove unused project files

--- src/vendors/types.ts
+++
@@ -1,7 +0,0 @@
-import { CommandPayload, DiscoveryResult } from '../transports/types';
-
-export interface VendorClient {
-  discover(): Promise<DiscoveryResult[]>;
-  execute(deviceId: string, payload: CommandPayload): Promise<void>;
-  syncState?(deviceId: string): Promise<Record<string, unknown>>;
-}
